46d9c749c8af1d604329cdcdfff160e88641e5e5
Misha <mihasyaterminator@gmail.com>
2020-08-29 12:34:07 +0300

    Filled DLLExport class

--- OpenLL/DLLExport.h
+++ OpenLL/DLLExport.h
@@ -1,10 +1,34 @@
 #pragma once
+#include <string>
+#include <iostream>
+#include <Windows.h>
 
-template <class T> class DLLExport
+using namespace std;
+
+template <class T, class ...Params>
+class DLLExport
 {
 public:
-	
-private:
+	DLLExport(string dll_name, string func_name)
+	{
+		wstring local_dll_name(dll_name.begin(), dll_name.end());
+		dll = LoadLibrary((LPCWSTR)local_dll_name.c_str());
 
+		auto temp_func = (T(*)(Params...))GetProcAddress(dll, func_name.c_str());
+		func = (FARPROC)temp_func;
+	}
+
+	T operator()(Params... params)
+	{
+		auto temp = (T(*)(Params...))func;
+		return temp(params...);
+	}
+	
+	
+
+private:
+	HMODULE dll;
+	FARPROC func;
+	
 };
 
--- OpenLL/main.cpp
+++ OpenLL/main.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
+#include <string>
+#include <Windows.h>
+#include "DLLExport.h"
 
 using namespace std;
 
 int main(int argc, char** argv)
 {
 	cout << "Hello World!" << endl;
+	// TODO: call {int sum(int, int)} func with DLLExport from "test_dll.dll"
+	DLLExport<int, int, int> func("test_dll.dll", "sum");
+	
+	cout << func(1, 1) << endl;
+
 	
 
 	return 0;
